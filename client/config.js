014cce11af095372fcb0867e1bba6314a46b441d
romennts <romennts@gmail.com>
2017-09-08 19:04:47 +0800

    delete client file

--- client/config.js
+++
@@ -1,6 +0,0 @@
-var cfg = {
-    apigwlink: 'http://XXXXX.com',
-    accesstoken: 'ABCDEEF',
-    serialNum: '12345'
-};
-module.exports = cfg;
--- client/index.js
+++
@@ -1,7 +0,0 @@
-var cfg = require('./config');
-var applycert = require('./applycert');
-console.log('Applying...');
-console.log('Please wait...\n');
-var i = 0 ;
-for (i;i<1000;i++)
-    applycert(cfg.accesstoken,cfg.serialNum);
